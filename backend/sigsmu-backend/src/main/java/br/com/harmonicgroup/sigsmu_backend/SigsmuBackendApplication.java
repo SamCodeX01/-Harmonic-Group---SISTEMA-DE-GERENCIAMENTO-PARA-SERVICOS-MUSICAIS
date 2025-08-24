722052b85cef29684c5f7c0e83c4d236e3a51d41
SamCodex01 <txtstring@gmail.com>
2025-08-24 18:32:19 -0300

    Adicionando projeto ao github

---
+++ backend/sigsmu-backend/src/main/java/br/com/harmonicgroup/sigsmu_backend/SigsmuBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.harmonicgroup.sigsmu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigsmuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigsmuBackendApplication.class, args);
+	}
+
+}
---
+++ backend/sigsmu-backend/src/test/java/br/com/harmonicgroup/sigsmu_backend/SigsmuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.harmonicgroup.sigsmu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigsmuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
