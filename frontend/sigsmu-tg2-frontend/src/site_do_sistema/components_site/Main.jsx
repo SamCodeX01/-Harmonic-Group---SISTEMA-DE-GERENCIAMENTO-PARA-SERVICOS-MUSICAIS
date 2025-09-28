fd56a15320bfb8b6ce81a557cabc9b66d3190228
SamCodex01 <txtstring@gmail.com>
2025-09-28 01:35:11 -0300

    Atualizei o componente Main do Site

--- frontend/sigsmu-tg2-frontend/src/site_do_sistema/components_site/Main.jsx
+++ frontend/sigsmu-tg2-frontend/src/site_do_sistema/components_site/Main.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import bgazulescuro from "../imagens_site/bgazulescuro.png"
 import maincss from "./main.module.css"
 
+//#196DBA - AZUL
+
 function Main(){
 
 return(
     <main className={maincss.main}>
         {/* <img src={bgazulescuro} alt="" srcset=""/> */}
-        <p>xxxxxxxuuu</p>
+        <p style={{color: "white"}}>Realize a festa dos seus sonhos e encante-se com músicas de qualidade!</p>
+        <button>Faça um orçamento</button>
     </main>
 );
 
