17822725187422b8df0a95a1db51696cf8d79085
SamCodex01 <txtstring@gmail.com>
2025-09-29 00:26:44 -0300

    Autalizando o Header do Site

--- frontend/sigsmu-tg2-frontend/src/site_do_sistema/components_site/Header.jsx
+++ frontend/sigsmu-tg2-frontend/src/site_do_sistema/components_site/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import headercss from "./header.module.css";
 import apagarDepois from "../image.png"
 import logotipo from "../imagens_site/logotipo300px.png"
+import { useNavigate } from "react-router-dom";
 
 function Header(){
+
+const navigate = useNavigate();
 
 return(
     <header className={headercss.header}>
@@ -15,7 +18,7 @@ return(
                 <li><a href="#">Orçamentos</a></li>
                 <li><a href="#">Contatos</a></li>
                 <li><a href="#">Seja Parceiro</a></li>
-                <li><a href="#">Intranet</a></li>
+                <li onClick={()=>navigate("/TelaLogin/")}><a href="#">Intranet</a></li>
             </ul>
                 {/* <button style={{background:"#007bff", color:"white", padding: "15px"}} >Intranet</button> */}
         </nav>
