9c97eddf2e82efb49cb9e41ca3b8761cec30d802
SamCodex01 <txtstring@gmail.com>
2025-11-15 22:50:18 -0300

    TipoLocalService.js atualizado

--- frontend/sigsmu-tg2-frontend/src/services/BancoDadosGestor/tipoLocalService.js
+++ frontend/sigsmu-tg2-frontend/src/services/BancoDadosGestor/tipoLocalService.js
@@ -4,3 +4,4 @@ export const listarTiposLocal = () => api.get("/tipolocal");
 export const adicionarTipoLocal = (body) => api.post("/tipolocal", body);
 export const atualizarTipoLocal = (id, body) => api.patch(`/tipolocal/${id}`, body);
 export const excluirTipoLocal = (id) => api.delete(`/tipolocal/${id}`);
+//apagar
